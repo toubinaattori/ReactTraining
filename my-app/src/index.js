a90bb30c385914ef622de471bdb1b6a14802f5ad
Topias Terho <topias.terho@lekane.com>
2018-07-04 15:52:26 +0300

    add buttons to page that expand while clicked

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,23 +5,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Day extends React.Component{
+
   constructor(props){
     super(props);
     this.state = {
-      day: "Monday",
-      temperature: "27",
+      day: null,
+      temperature: null,
+    }
+  }
+  render() {
+    return (
+      <button className="day" onClick={() => this.setState({day: this.props.value})}>
+      {this.state.day}
+    </button>
+    );
+  }
+}
+
+class DayRow extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      days: Array(7).fill(null),
     }
   }
 
-    render() {
   
-      return (
-        <div>
-          <div className="day">{this.state.day}</div>
-          <div className="temperature">{this.state.temperature}</div>
-        </div>
-      );
-    }
+
+  renderDay(i,day){
+    return <Day value={day} />;
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <div className="day-row">{this.renderDay(0,"Monday")}{this.renderDay(1,"Tuesday")}
+        {this.renderDay(2,"Wednesday")}{this.renderDay(3,"Thursday")}{this.renderDay(4,"Friday")}
+        {this.renderDay(5,"Saturday")}{this.renderDay(6,"Sunday")}</div>
+      </div>);
+  }
 }
 
 
@@ -32,7 +56,7 @@ class Weather extends React.Component {
     return (
       <div className="weather">
         <div className="weather-panel">
-        <Day/>
+        <DayRow/>
         </div>
         <div className="day">
         </div>
